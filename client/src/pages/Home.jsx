4f9ed91f1c4e55e922fd61a1b8a2b714c31b760a
Prakash Adhikari <pa7846665@gmail.com>
2023-10-30 18:20:32 +0530

    mern auth

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='text-2xl font-extralight'>helmooooooo ji adhikari here </div>
+    )
+}
+
+export default Home
